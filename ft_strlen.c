ec511e47e820e5b2083287b4d5dccb1e4f94588e
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-04 13:45:12 +0200

    ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,29 +6,18 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 09:53:15 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/23 10:49:28 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/04 13:42:00 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <string.h>
-#include <stdio.h>
 
 int		ft_strlen(const char *s)
 {
 	size_t i;
-	
+
 	i = 0;
-	while(s[i])
+	while (s[i])
 	{
 		i++;
 	}
-	return(i);
-
-}
-
-int		main()
-{
-	char str[15] = "hello world";
-	printf("%d\n", ft_strlen(str));
-	return(0);
+	return (i);
 }
