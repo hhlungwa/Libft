bb559484a770e887ba8fd1998d0f302762df405e
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-04 13:25:05 +0200

    ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,12 +6,9 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:28:33 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/23 09:40:18 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/04 13:22:37 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <string.h>
-#include <stdio.h>
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -25,14 +22,4 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	while(i < n && 	str1[i] == str2[i])
 		i++;
 	return(str1[i] - str2[i]);	
-}
-
-int		main()
-{
-	char str1[15] = "hello world";
-	char str2[15] = "hello Hitekani";
-	int ret;
-
-	ret = ft_memcmp(str1, str2, 11);
-	printf("%d\n", ret);
 }
