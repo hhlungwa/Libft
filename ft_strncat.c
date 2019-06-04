e3d178b81155addf905bf23feda7be1093ffa456
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-04 16:30:44 +0200

    ft_strncat.c

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,42 +6,25 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:09:57 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/24 12:01:58 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/04 16:29:32 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
-char	*ft_strncat(char *s1, char *s2, size_t n)
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
 	size_t j;
 	size_t i;
 
 	i = 0;
-	while(s1[i])
-		i++;	
+	while (s1[i])
+		i++;
 	j = 0;
-
-	while(j < n)
+	while (j < n)
 	{
 		s1[i] = s2[j];
 		j++;
 		i++;
 	}
 	s1[i] = '\0';
-	return(s1);
-}
-
-int		main()
-{
-	char str1[20] = "Hello";
-	char str2[20] = "Deshey";
-	char *ret;
-
-	ret = ft_strncat(str2, str1, 5);
-	printf("%s\n", ret);
-	return(0);
-
-
+	return (s1);
 }
