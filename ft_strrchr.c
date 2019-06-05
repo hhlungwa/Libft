7acc43cb842d97ecb69f54ad514a14a2599a476a
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-05 11:43:47 +0200

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,41 +6,26 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 09:37:44 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/28 10:18:26 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/05 11:42:13 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
 char		*ft_strrchr(char *s, int c)
 {
-	char a;
-	int i;
+	char	a;
+	int		i;
 
-	a = (int) c;
+	a = (int)c;
 	i = 0;
-
-	while(s[i])
+	while (s[i])
 		i++;
-
-	while(i >= 0)
+	while (i >= 0)
 	{
 		if (s[i] == c)
 		{
-			return (&s[i]);
+			return (s + i);
 		}
 		i--;
 	}
-	return(NULL);
-}
-
-int		main()
-{
-	char str1[15] = "hallo dashey";
-	char *ret;
-
-	ret = ft_strrchr(str1, 97);
-	printf("%s", ret);
-	return(0);
+	return (NULL);
 }
