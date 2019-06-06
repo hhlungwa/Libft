9756ed9e47008f96774b1cc7fb2c0971161773e8
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 11:31:30 +0200

    ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,12 +6,9 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:16:10 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/27 15:08:04 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 11:29:19 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <ctype.h>
-#include <stdio.h>
 
 int		ft_isascii(int c)
 {
@@ -21,12 +18,3 @@ int		ft_isascii(int c)
 		return(0);
 }
 
-int		main(void)
-{
-	int str = '/';
-	int ret;
-
-	ret = ft_isascii(str);
-	printf("%d", ret);
-	return(0);
-}
