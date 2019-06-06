b922730ab5c522021caae2eb4c9a7d842f70d681
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 11:47:13 +0200

    ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,28 +6,14 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:51:08 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/27 16:00:16 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 11:46:16 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <ctype.h>
-#include <stdio.h>
 
 int		ft_tolower(int c)
 {
 	if(c >= 'A' && c <= 'Z')
-		return(c + 32);
+		returni	(c + 32);
 	else
-		return(c);
-}
-
-int		main()
-{
-	int str = 'A';
-	char ret;
-
-	ret = ft_tolower(str);
-	printf("%c", ret);
-	return(0);
-
+		returni	(c);
 }
