b8da7119b08a68f788599c96e6bbd77cce68412e
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 13:38:13 +0200

    ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,42 +6,28 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:16:13 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/21 15:35:29 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 13:36:11 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	char *str1;
-	char *str2;
-	size_t i;
+	char	*str1;
+	char	*str2;
+	size_t	i;
 
-	str1 = (char *) dest;
-	str2 = (char *) src;
+	str1 = (char *)dest;
+	str2 = (char *)src;
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 			str1[i] = str2[i];
-			if(str2[i] == c)
+			if (str2[i] == c)
 			{
-				i++; //loops until the c character
+				i++;
 				break;
 			}
 			i++; 
 	}
-	str1[i] = '\0';
-	return(str1);
+	return (str1);
 }	
-
-int main()
-{
-	char str1[15] = "Hi Deshey";
-	char str2[15] = "think: enough";
-
-	ft_memccpy(str1, str2, ':', 14);
-	printf("%s\n", str1);
-
-}
