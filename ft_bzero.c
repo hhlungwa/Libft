658347b3c94803f5d598579ea31067433c6d4525
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 11:58:21 +0200

    ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,32 +6,22 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:48:59 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/21 11:50:07 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 11:56:50 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
 void	ft_bzero(void *s, size_t n)
 {
-	char *str;
-	size_t i;
+	char	*str;
+	size_t	i;
 
-	str = (char *) s;
+	str = (char *)s;
 	i = 0;
 
-	while(i < n)
+	while (i < n)
 	{	
-		str[i] = '0';
+		str[i] = 0;
 		i++;
 	
 	}
-}
-
-int		main()
-{
-	char str[15] = "hello world";
-	ft_bzero(str, 5);
-	printf("%s\n", str);
 }
