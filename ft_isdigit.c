1e3ecc7033012ec9df923815e45bd94f4076703d
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 11:17:18 +0200

    ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,12 +6,9 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 13:32:27 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/27 13:43:14 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 11:12:04 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <ctype.h>
-#include <stdio.h>
 
 int		ft_isdigit(int c)
 {
@@ -21,12 +18,3 @@ int		ft_isdigit(int c)
 		return(0);		
 }
 
-int		main(void)
-{
-	int str = 'a';
-	int ret;
-
-	ret = ft_isdigit(str);
-	printf("%d", ret);
-	return(0);
-}
