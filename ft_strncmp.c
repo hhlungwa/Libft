4d34ffb04a195880efb7af215a15bc38a9ab1c85
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-06 10:14:41 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,33 +6,18 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 11:22:57 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/05/27 11:39:16 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/06 10:07:25 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
 int		ft_strncmp(char *s1, char *s2, size_t len)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
-	while( i < len && s1[i] == s2[i])
+	while (i < len && s1[i] == s2[i])
 	{
 		i++;
 	}
-	return(s1[i] - s2[i]);
-
-}
-
-int		main()
-{
-	char str[15] = "Deshey";
-	char str1[15] = "world";
-	int ret;
-
-	ret = ft_strncmp(str, str1, 5);
-	printf("%d", ret);
-	return(0);
+	return (s1[i] - s2[i]);
 }
