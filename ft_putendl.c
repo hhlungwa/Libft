3472eeda69f7eaee136b0f2dc8232bf9c039fa5e
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-11 15:23:39 +0200

    ft_putendl.c

--- ft_putendl.c
+++ ft_putendl.c
@@ -6,36 +6,21 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:47:34 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/06/11 12:01:12 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/11 15:18:49 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <string.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return;
-}
-
-void	ft_putstr(char const *str)
-{
-	size_t i;
-
-	i = 0;
-	while(str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-	return;
-}
-
 void	ft_putendl(char const *s)
 {
-		ft_putstr(s);
-		ft_putchar('\n');
+		int i;
+
+		i = 0;
+		while(s[i])
+			write(1, &s[i++], 1);
+		write(1, "\n", 1);
 }
 
 int		main()
