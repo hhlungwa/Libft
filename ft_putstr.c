263968cfe34f9247499030be2c954f4690e38405
Hitekani HLUNGWANI <hhlungwa@c4r7s1.wethinkcode.co.za>
2019-06-11 14:50:14 +0200

    ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,23 +6,17 @@
 /*   By: hhlungwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:02:43 by hhlungwa          #+#    #+#             */
-/*   Updated: 2019/06/11 10:27:52 by hhlungwa         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:47:57 by hhlungwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_putstr(char const *s)
 {
-
-	while(*s)
-		ft_putchar(*s++);
+	int i;
+	
+	i = 0;
+	while(s[i])
+		write(fd, &s[i++], 1);
 }
 
 int		main()
